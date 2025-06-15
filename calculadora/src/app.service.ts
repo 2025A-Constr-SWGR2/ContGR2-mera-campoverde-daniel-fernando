019fb4eed1a45ae6f3ada6ab31be63c6901693c8
DANIEL FERNANDO MERA CAMPOVERDE <daniel.mera01@gmail.com>
2025-06-15 02:09:33 -0500

    git actions

---
+++ calculadora/src/app.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    //console.log('log!!');
+    //console.debug('debug!!');
+    //console.info('info!!');
+    //console.warn('warn!!');
+    //console.error('error!!');
+    return 'Hello World!';
+  }
+}
